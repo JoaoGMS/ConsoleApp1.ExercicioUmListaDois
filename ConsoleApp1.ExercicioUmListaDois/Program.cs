63078b9916285c8647ad97d71858ee28afd7c8f5
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 17:08:37 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioUmListaDois/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioUmListaDois
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício um // Lista dois  ");
+            
+            
+                // Console.WriteLine("");
+
+                double X, Y, Z;
+
+                Console.WriteLine("Digite o valor de X: ");
+                X = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o valor de Y: ");
+                Y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o valor de Z: ");
+                Z = Convert.ToDouble(Console.ReadLine());
+
+                if (X + Y < Z) 
+            
+                Console.WriteLine("A soma de X e Y é menor que Z");
+
+
+            
+                else
+                    Console.WriteLine("A soma de X e Y é maior que Z");
+
+             
+
+        }
+    }
+}
